5063a3e837ec7ff1087369eb6d787ca7b45769ef
le-minhphuc <leminhphuc10t1@gmail.com>
2020-04-26 11:03:48 +0800

    Clean up unnecessary partial template specialization and member (#2)
    * Remove unnecessary partial template specialization of Node
    
    * Remove unnecessary member _validResult
    
    std::optional gives us the capability to check if our result storage is
    valid for collection.

--- graphex.hpp
+++ graphex.hpp
@@ -51,13 +51,23 @@ protected:
     bool _isOutput = false;
 };
 
-template <typename ReturnType, typename... Args>
+template <typename TaskCallback, typename... Args>
 class Node final : public BaseNode {
     friend class GraphEx;
-
 public:
-    using TaskCallback = std::function<ReturnType(Args...)>;
-    using SubscribeCallback = std::function<void(ReturnType)>;
+    using ReturnType = std::invoke_result_t<TaskCallback, Args...>;
+    using ResultStorage = typename std::conditional_t<
+        !std::is_void_v<ReturnType>,
+        ReturnType,
+        int /* placeholder type for void-returning function */
+        >;
+    using SubscribeCallback = std::function<
+        void(typename std::conditional_t<
+            !std::is_void_v<ReturnType>,
+            ReturnType,
+            std::false_type
+            >)
+        >;
     using SubscribeNoArgCallback = std::function<void(void)>;
 
     Node(TaskCallback task) : _task(task) {}
@@ -83,6 +93,9 @@ public:
     template <std::size_t idx, typename ParentTask>
     void setParent(ParentTask& parent)
     {
+        static_assert(!std::is_same_v<typename ParentTask::ReturnType, void>,
+            "Could not record result of a function that returns void as "
+            "an argument for this task");
         parent.addChild(
             std::bind(&Node::onArgumentReady<idx>, this,
                 std::placeholders::_1
@@ -116,28 +129,36 @@ public:
     /// After the _task is finish, call the registered callback functions
     /// @throw if `ReturnType` is non-copyable but there are more than 1 child
     /// tasks that require the result object
-    void execute() override
+    virtual void execute() override
     {
         // wait for result to be ready
         while (_pendingCount > 0) ;
-        if constexpr (!std::is_copy_constructible<ReturnType>::value) {
-            DE_ENFORCE(_childTasks.size() <= 1,
-                       "Internal Error: More than 1 child process for "
-                       "non-copyable object");
-            _result = std::apply(_task, std::move(_args));
-            _validResult = true;
-            if (!_childTasks.empty()) {
-                _childTasks[0](std::move(_result.value()));
-                _validResult = false;
+        if constexpr (std::is_void_v<ReturnType>) {
+            if constexpr (!std::is_copy_constructible<decltype(_args)>::value) {
+                std::apply(_task, std::move(_args));
             }
+            else
+                std::apply(_task, _args);
         }
         else {
-            _result = std::apply(_task, _args);
-            _validResult = true;
-            for (size_t i = 0; i < _childTasks.size(); ++i) {
-                _childTasks[i](_result.value());
+            if constexpr (!std::is_copy_constructible<ReturnType>::value) {
+                DE_ENFORCE(_childTasks.size() <= 1,
+                        "Internal Error: More than 1 child process for "
+                        "non-copyable object");
+                _result = std::apply(_task, std::move(_args));
+                if (!_childTasks.empty()) {
+                    _childTasks[0](std::move(_result.value()));
+                    _result.reset();
+                }
+            }
+            else {
+                _result = std::apply(_task, _args);
+                for (size_t i = 0; i < _childTasks.size(); ++i) {
+                    _childTasks[i](_result.value());
+                }
             }
         }
+
         for (auto childTask : _noArgChildTasks) childTask();
     }
 
@@ -146,7 +167,7 @@ public:
     /// node
     ReturnType collect()
     {
-        if (!_validResult) {
+        if (!_result) {
             throw std::runtime_error("No result found in node");
         }
         if constexpr (!std::is_copy_constructible<ReturnType>::value) {
@@ -221,8 +242,7 @@ private:
 
     TaskCallback _task;
     std::tuple<Args...> _args;
-    bool _validResult = false;
-    std::optional<ReturnType> _result;
+    std::optional<ResultStorage> _result;
 
     size_t _parentEnqueued = 0;
     std::atomic<size_t> _pendingCount = std::tuple_size<std::tuple<Args...>>::value;
@@ -230,126 +250,6 @@ private:
 
     std::vector<SubscribeCallback> _childTasks;
     std::vector<SubscribeNoArgCallback> _noArgChildTasks;
-};
-
-template <typename... Args>
-class Node<void, Args...> final : public BaseNode {
-    friend class GraphEx;
-
-public:
-    using TaskCallback = std::function<void(Args...)>;
-    using SubscribeNoArgCallback = std::function<void(void)>;
-
-    Node(TaskCallback task) : _task(task) {}
-
-    /// @brief setParent add a node as a prequel to current node, and the
-    /// result of parent node will be passed or consumed by current node once
-    /// the parent node _task is done
-    /// @param idx template argument of the position of result object in current
-    /// node's `_args` For example, let's say there are 3 functions, whose
-    /// signatures as followed
-    /// ```
-    /// a = []() -> int {}
-    /// b = []() -> int {}
-    /// c = [](int x, int y) -> int {}
-    /// ```
-    /// We need to know whether the result returned by a is passed as `x` or `y`
-    /// to c. Using `setParent`, we can specify by
-    /// ```
-    /// nodeC->setParent<0>(*nodeA) // if result by a is passed as x or
-    /// nodeC->setParent<1>(*nodeA) // if result by a is passed as y
-    /// ```
-    /// @param parent parent node to be added
-    template <std::size_t idx, typename ParentTask>
-    void setParent(ParentTask& parent)
-    {
-        parent.addChild(std::bind(&Node::onArgumentReady<idx>, this,
-                                   std::placeholders::_1));
-        parent._nextNodes.emplace_back(this);
-    }
-
-    /// @brief setParent add a node as a prequel to current node. The parent
-    /// won't be passing any result needed by current node, but it still
-    /// requires the parent node _task to run first before running. For example,
-    /// ```
-    /// a = []() -> int {}
-    /// b = []() -> int {}
-    /// nodeA->setParent(*nodeB);
-    /// ```
-    /// @param parent parent node to be added
-    template <typename ParentTask>
-    void setParent(ParentTask& parent)
-    {
-        addParentCount();
-        SubscribeNoArgCallback cb = std::bind(
-            static_cast<void (Node::*)(void)>(&Node::onArgumentReady), this);
-        parent.addChild(cb);
-        parent._nextNodes.emplace_back(this);
-    }
-
-    /// @brief Run the main _task registered by current node
-    /// After the _task is finish, call the registered callback functions
-    void execute() override
-    {
-        while (_pendingCount > 0);
-        if constexpr (!std::is_copy_constructible<decltype(_args)>::value) {
-            std::apply(_task, std::move(_args));
-        }
-        else
-            std::apply(_task, _args);
-        for (auto childTask : _childTasks) childTask();
-    }
-
-    void collect() {}
-
-    /// @brief Add a callback function that will be called when the _task in
-    /// current node finished running. The object generated by the _task in thise
-    /// node will be passed to or consumed by the callback function `child`
-    /// @param child callback function
-    void addChild(SubscribeNoArgCallback child)
-    {
-        _childTasks.push_back(child);
-    }
-
-private:
-    virtual void parentEnqueued() override { ++_parentEnqueued; }
-    virtual bool allParentEnqueued() override
-    {
-        return _parentEnqueued == _parentCount;
-    }
-    void addParentCount()
-    {
-        ++_parentCount;
-        ++_pendingCount;
-    }
-    virtual void reset() override
-    {
-        _pendingCount = _parentCount;
-        _parentEnqueued = 0;
-    }
-    /// @brief A register function that can be used to register callback when
-    /// parent nodes have finish running
-    template <std::size_t idx>
-    auto onArgumentReady(decltype(
-        std::get<idx>(std::declval<std::tuple<Args...>>())) arg) -> void
-    {
-        if constexpr (!std::is_copy_constructible<decltype(arg)>::value) {
-            std::get<idx>(_args) = std::move(arg);
-        }
-        else
-            std::get<idx>(_args) = arg;
-        --_pendingCount;
-    }
-
-    void onArgumentReady() { --_pendingCount; }
-    TaskCallback _task;
-    std::tuple<Args...> _args;
-
-    size_t _parentEnqueued = 0;
-    std::atomic<size_t> _pendingCount = std::tuple_size<std::tuple<Args...>>::value;
-    size_t _parentCount = std::tuple_size<std::tuple<Args...>>::value;
-
-    std::vector<SubscribeNoArgCallback> _childTasks;
 };
 
 class GraphEx {
@@ -364,12 +264,7 @@ public:
     }
 
     template <typename... Args>
-    void registerInputNode(Node<void, Args...>* node)
-    {
-        _inputNodes.emplace_back(node);
-    }
-
-    void registerInputNode(Node<void>* node)
+    void registerInputNode(Node<Args...>* node)
     {
         _inputNodes.emplace_back(node);
     }
@@ -460,24 +355,18 @@ private:
 template <typename ReturnType, typename... Args>
 decltype(auto) makeNode(std::function<ReturnType(Args...)> func)
 {
-    return Node<ReturnType, Args...>(func);
+    return Node<std::function<ReturnType(Args...)>, Args...>(func);
 }
 
 template <typename... Args>
 decltype(auto) makeNode(std::function<void(Args...)> func)
 {
-    return Node<void, Args...>(func);
+    return Node<std::function<void(Args...)>, Args...>(func);
 }
 
-template <typename ReturnType>
-decltype(auto) makeNode(std::function<ReturnType(void)> func)
+decltype(auto) makeNode(std::function<void()> func)
 {
-    return Node<ReturnType>(func);
-}
-
-decltype(auto) makeNode(std::function<void(void)> func)
-{
-    return Node<void>(func);
+    return Node<std::function<void()>>(func);
 }
 
 }  // namespace GE
